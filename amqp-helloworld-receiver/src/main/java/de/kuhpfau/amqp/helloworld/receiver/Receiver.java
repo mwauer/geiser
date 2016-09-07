095494b78b3fdc297bda21b68a222ebd3f8f5785
mwauer <dr.matthias.wauer@gmail.com>
2016-09-07 13:20:20 +0200

    Fixed uninitialized hello queue issue

--- amqp-helloworld-receiver/src/main/java/de/kuhpfau/amqp/helloworld/receiver/Receiver.java
+++ amqp-helloworld-receiver/src/main/java/de/kuhpfau/amqp/helloworld/receiver/Receiver.java
@@ -2,7 +2,9 @@ package de.kuhpfau.amqp.helloworld.receiver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,11 @@ public class Receiver {
 	
 	private final static Logger Log = LoggerFactory.getLogger(Receiver.class);
 
+	@Bean
+	public Queue helloQueue() {
+		return new Queue("hello", false, false, true);
+	}
+	
 	@RabbitListener(queues="hello")
 	public void handleMessage(HelloMessageBean bean) {
 		Log.info("Got Hello "+bean.hello);
