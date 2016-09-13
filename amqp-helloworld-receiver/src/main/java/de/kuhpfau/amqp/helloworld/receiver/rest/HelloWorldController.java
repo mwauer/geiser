8b158220cc1de5a76602bbcd45ecd45832afc196
mwauer <dr.matthias.wauer@gmail.com>
2016-09-13 10:10:15 +0200

    added some test REST controller and configured in spring boot

---
+++ amqp-helloworld-receiver/src/main/java/de/kuhpfau/amqp/helloworld/receiver/rest/HelloWorldController.java
@@ -0,0 +1,21 @@
+package de.kuhpfau.amqp.helloworld.receiver.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public ResponseEntity<String> handle(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return new ResponseEntity<String>("Hello "+name, HttpStatus.OK);
+    }
+
+}
