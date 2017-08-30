5d58533b774c5f5c89b1cfe03c77b782e529dd22
mwauer <dr.matthias.wauer@gmail.com>
2017-08-30 17:31:16 +0200

    Added TomTom test case for sparqlify service

--- geiser-sparqlify-service/src/test/java/org/aksw/geiser/sparqlify/SparqlifyRequestProcessorTest.java
+++ geiser-sparqlify-service/src/test/java/org/aksw/geiser/sparqlify/SparqlifyRequestProcessorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import java.io.IOException;
 
 import org.antlr.runtime.RecognitionException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,8 +15,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
@@ -27,21 +30,39 @@ public class SparqlifyRequestProcessorTest {
 	@Value("classpath:/input.json")
 	private Resource input;
 
+	@Value("classpath:/input-fcd.json")
+	private Resource inputFcd;
+
 	@Test
-	public void testProcess() throws JsonProcessingException, IOException, RecognitionException {
-		log.info("{}", input);
-
-		ObjectMapper jsonObjectMapper = new ObjectMapper();
-		jsonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-		SparqlifyServiceRequest request = jsonObjectMapper.readValue(input.getInputStream(),
-				SparqlifyServiceRequest.class);
+	public void testProcessSml() throws JsonProcessingException, IOException, RecognitionException {
+		SparqlifyServiceRequest request = readRequest(input);
 		log.info("{}", request);
 
 		String result = new SparqlifyRequestProcessor().process(request);
 
 		log.info("result: {}", result);
 		assertNotNull(result);
+	}
+	
+	@Test
+	public void testProcessR2rml() throws JsonParseException, JsonMappingException, IOException, RecognitionException {
+		SparqlifyServiceRequest request = readRequest(inputFcd);
+		log.info("{}", request);
+
+		String result = new SparqlifyRequestProcessor().process(request);
+
+		log.info("result: {}", result);
+		assertNotNull(result);
+		
+	}
+	
+	private SparqlifyServiceRequest readRequest(Resource input) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper jsonObjectMapper = new ObjectMapper();
+		jsonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		SparqlifyServiceRequest request = jsonObjectMapper.readValue(input.getInputStream(),
+				SparqlifyServiceRequest.class);
+		return request;
 	}
 
 }
