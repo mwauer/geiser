55a61f16ff3bb36763f4b6898cbac66380e61b6a
mwauer <dr.matthias.wauer@gmail.com>
2018-09-19 12:16:22 +0200

    Completed NER service, untested

--- geiser-ner-service/src/main/java/org/aksw/geiser/ner/GeiserNERApplication.java
+++ geiser-ner-service/src/main/java/org/aksw/geiser/ner/GeiserNERApplication.java
@@ -1,16 +1,21 @@
 package org.aksw.geiser.ner;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
+import org.aksw.geiser.util.Message2Rdf4jModel;
 import org.aksw.geiser.util.ServiceUtils;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.util.Models;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -30,6 +35,9 @@ import org.springframework.stereotype.Component;
  */
 @SpringBootApplication
 public class GeiserNERApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(GeiserNERApplication.class);
+
 
 	@Autowired
 	private SimpleNERWrapper wrapper;
@@ -50,21 +58,28 @@ public class GeiserNERApplication {
 
 		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
 		public void handleTestMessage(@Payload Message payload) throws IOException {
-			System.out.println("Got test message: " + new String(payload.getBody()));
-
-			List<IRI> entities = wrapper.getEntities("test");
+			log.debug("Got NER message: {}", new String(payload.getBody()));
 			
-			ModelBuilder modelBuilder = new ModelBuilder();
+			Model model = Message2Rdf4jModel.convertToModel(payload);
+
+			Optional<org.eclipse.rdf4j.model.Value> textObject = Models.object(model.filter(null, SimpleValueFactory.getInstance().createIRI("http://rdfs.org/sioc/ns#content"), null));
+			
+			List<IRI> entities = wrapper.getEntities(textObject.get().stringValue());
+			
+			ModelBuilder modelBuilder = new ModelBuilder(model);
 			//modelBuilder.subject(subject);
 			for (IRI entity : entities) {
+				log.debug("Adding NER entity: {}", entity);
 				modelBuilder.add(SimpleValueFactory.getInstance().createIRI("http://rdfs.org/sioc/ns#topic_dbpedia"), entity);				
 			}
+			
+			Message message = Message2Rdf4jModel.convertToMessage(model, RDFFormat.JSONLD);
+			
+			log.debug("Sending NER response: {}", new String(message.getBody()));
 
 			// for sending a response message to the next routing key:
-//			Message result = MessageBuilder.withBody("result".getBytes(StandardCharsets.UTF_8))
-//					.setContentType("text/plain;charset=utf-8").build();
-//			rabbitTemplate.send(message.getMessageProperties().getReceivedExchange(),
-//					ServiceUtils.nextRoutingKey(message), result);
+			rabbitTemplate.send(message.getMessageProperties().getReceivedExchange(),
+					ServiceUtils.nextRoutingKey(payload), message);
 
 		}
 	}
