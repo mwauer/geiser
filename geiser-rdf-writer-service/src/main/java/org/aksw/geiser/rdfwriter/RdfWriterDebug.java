1d8517010da560d8f88d7f9a311c1af61d637014
Johannes Trame <jt@metaphacts.com>
2018-10-14 20:52:16 +0200

    fix writer debug service

--- geiser-rdf-writer-service/src/main/java/org/aksw/geiser/rdfwriter/RdfWriterDebug.java
+++ geiser-rdf-writer-service/src/main/java/org/aksw/geiser/rdfwriter/RdfWriterDebug.java
@@ -1,19 +1,14 @@
 package org.aksw.geiser.rdfwriter;
 
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.query.QueryResults;
 import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.util.Repositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -32,9 +27,7 @@ public class RdfWriterDebug {
 	public void handleRdfWriterDebugMessage(
 			@Payload Message payload/* , @Headers Map<String, Object> headers */) {
 		log.info("RdfWriterDebug got message: {}", payload);
-		Model model = Repositories.graphQuery(repository, "CONSTRUCT WHERE { ?s ?p ?o }",
-				r -> QueryResults.asModel(r));
-		log.info("Store contents: {}", model);
+		log.info("RdfWriterDebug got body: {}", payload.getBody().toString());
 	}
 
 }
