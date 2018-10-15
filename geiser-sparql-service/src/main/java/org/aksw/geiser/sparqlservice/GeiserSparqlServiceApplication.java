91304d4a619d23ebda8d2b6c309386d535b710b8
Johannes Trame <jt@metaphacts.com>
2018-10-15 12:58:38 +0200

    refactor sparql operations into individual services

--- geiser-sparql-service/src/main/java/org/aksw/geiser/sparqlservice/GeiserSparqlServiceApplication.java
+++ geiser-sparql-service/src/main/java/org/aksw/geiser/sparqlservice/GeiserSparqlServiceApplication.java
@@ -8,6 +8,7 @@ import java.util.function.Supplier;
 import org.aksw.geiser.util.ServiceUtils;
 import org.eclipse.rdf4j.common.lang.FileFormat;
 import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.Update;
 import org.eclipse.rdf4j.query.resultio.BooleanQueryResultFormat;
 import org.eclipse.rdf4j.query.resultio.BooleanQueryResultWriterRegistry;
 import org.eclipse.rdf4j.query.resultio.QueryResultFormat;
@@ -46,7 +47,7 @@ import com.google.common.collect.Sets;
 public class GeiserSparqlServiceApplication {
 	private static final Logger log = LoggerFactory.getLogger(GeiserSparqlServiceApplication.class);
 	
-	enum Operation{ ASK, DESCRIBE,SELECT, CONSTRUCT, UPDATE};
+	enum Operation{ ASK, DESCRIBE, SELECT, CONSTRUCT, UPDATE};
 
 	@Autowired
 	private Repository repository;
@@ -56,32 +57,82 @@ public class GeiserSparqlServiceApplication {
 	private RabbitTemplate rabbitTemplate;
 	
 	@Component
-	public class GeiserSparqlServiceService {
+	public class GeiserSparqlAskService {
 		
-		public static final String ROUTING_KEY = "sparql-v1.#";
-		public static final String QUEUE_NAME = "sparql-v1";
+		public static final String ROUTING_KEY = "sparq-ask-v1.#";
+		public static final String QUEUE_NAME = "sparql-ask-v1";
 		
 		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
 		public void handleTestMessage(
 				@Payload Message payload, @Headers Map<String, String> headers) {
 			String sparqlQuery = new String(payload.getBody());
-			System.out.println("Got test message: "+sparqlQuery);
-			processSparqlQuery(payload, sparqlQuery, headers);
-
+			log.info("Got body (expecting SPARQL ASK query): "+sparqlQuery);
+			processSparqlQuery(payload, sparqlQuery, headers, Operation.ASK);
 		}
 	}
 
-	private void processSparqlQuery(Message message, String sparqlQuery, Map<String, String> headers){
-		String typeHeader = headers.get("OPERATION_TYPE");
-		String acceptHeader = headers.get("ACCEPT");
+	@Component
+	public class GeiserSparqlSelectService {
 		
-		Operation op = Operation.SELECT;
-		if(typeHeader == null || Operation.valueOf(typeHeader) == null){
-			log.warn("OPERATION_TYPE header is null or invalide value. {}", typeHeader);
-			log.warn("Using default \"SELECT\". ");
-		}else{
-			op = Operation.valueOf(typeHeader);
+		public static final String ROUTING_KEY = "sparql-select-v1.#";
+		public static final String QUEUE_NAME = "sparql-select-v1";
+		
+		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
+		public void handleTestMessage(
+				@Payload Message payload, @Headers Map<String, String> headers) {
+			String sparqlQuery = new String(payload.getBody());
+			log.info("Got body (expecting SPARQL SELECT query): "+sparqlQuery);
+			processSparqlQuery(payload, sparqlQuery, headers, Operation.SELECT);
 		}
+	}
+
+	@Component
+	public class GeiserSparqlConstructService {
+		
+		public static final String ROUTING_KEY = "sparql-construct-v1.#";
+		public static final String QUEUE_NAME = "sparql-construct-v1";
+		
+		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
+		public void handleTestMessage(
+				@Payload Message payload, @Headers Map<String, String> headers) {
+			String sparqlQuery = new String(payload.getBody());
+			log.info("Got body (expecting SPARQL CONSTRUCT query): "+sparqlQuery);
+			processSparqlQuery(payload, sparqlQuery, headers, Operation.CONSTRUCT);
+		}
+	}
+
+	@Component
+	public class GeiserSparqlDescribeService {
+		
+		public static final String ROUTING_KEY = "sparql-describe-v1.#";
+		public static final String QUEUE_NAME = "sparql-describe-v1";
+		
+		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
+		public void handleTestMessage(
+				@Payload Message payload, @Headers Map<String, String> headers) {
+			String sparqlQuery = new String(payload.getBody());
+			log.info("Got body (expecting SPARQL DESCRIBE query): "+sparqlQuery);
+			processSparqlQuery(payload, sparqlQuery, headers, Operation.DESCRIBE);
+		}
+	}
+
+	@Component
+	public class GeiserSparqlUpdateService {
+		
+		public static final String ROUTING_KEY = "sparql-update-v1.#";
+		public static final String QUEUE_NAME = "sparql-update-v1";
+		
+		@RabbitListener(bindings = @QueueBinding(key = ROUTING_KEY, exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "geiser", durable = "true", autoDelete = "true"), value = @org.springframework.amqp.rabbit.annotation.Queue(autoDelete = "true", value = QUEUE_NAME)))
+		public void handleTestMessage(
+				@Payload Message payload, @Headers Map<String, String> headers) {
+			String sparqlQuery = new String(payload.getBody());
+			log.info("Got body (expecting SPARQL UPDATE query): "+sparqlQuery);
+			processSparqlQuery(payload, sparqlQuery, headers, Operation.UPDATE);
+		}
+	}
+
+	private void processSparqlQuery(Message message, String sparqlQuery, Map<String, String> headers, Operation op){
+		String acceptHeader = headers.get("ACCEPT");
 		
 		FileFormat returnFormat = null;
 		Set<FileFormat> allFormats = Sets.newLinkedHashSet();
@@ -94,67 +145,71 @@ public class GeiserSparqlServiceApplication {
         
 		try{
 			
-		if (op.equals(Operation.ASK)) {
-			returnFormat = BooleanQueryResultFormat.matchMIMEType(acceptHeader,
-					allFormats).orElseGet(
-					new Supplier<BooleanQueryResultFormat>() {
-						@Override
-						public BooleanQueryResultFormat get() {
-							log.warn(
-									"ACCEPT header is empty or unkown. Fallback to {}.",
-									BooleanQueryResultFormat.TEXT
-											.getDefaultMIMEType());
-							return BooleanQueryResultFormat.TEXT;
-						}
-					});
-			try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
-				try(RepositoryConnection con = repository.getConnection()){
-					QueryResultIO.writeBoolean(con.prepareBooleanQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (QueryResultFormat) returnFormat, boa);
+			if (op.equals(Operation.ASK)) {
+				returnFormat = BooleanQueryResultFormat.matchMIMEType(acceptHeader,
+						allFormats).orElseGet(
+						new Supplier<BooleanQueryResultFormat>() {
+							@Override
+							public BooleanQueryResultFormat get() {
+								log.warn(
+										"ACCEPT header is empty or unkown. Fallback to {}.",
+										BooleanQueryResultFormat.TEXT
+												.getDefaultMIMEType());
+								return BooleanQueryResultFormat.TEXT;
+							}
+						});
+				try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
+					try(RepositoryConnection con = repository.getConnection()){
+						QueryResultIO.writeBoolean(con.prepareBooleanQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (QueryResultFormat) returnFormat, boa);
+					}
+					writeResult(message, boa, returnFormat);
 				}
-				writeResult(message, boa, returnFormat);
-			}
-		} else if (op.equals(Operation.CONSTRUCT) || op.equals(Operation.DESCRIBE)) {
-			returnFormat = RDFFormat.matchMIMEType(acceptHeader, allFormats)
-					.orElseGet(new Supplier<RDFFormat>() {
-						@Override
-						public RDFFormat get() {
-							log.warn(
-									"ACCEPT header is empty or unkown. Fallback to {}.",
-									RDFFormat.JSONLD.getDefaultMIMEType());
-							return RDFFormat.JSONLD;
-						}
-					});
-			try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
-				try(RepositoryConnection con = repository.getConnection()){
-					QueryResultIO.writeGraph(con.prepareGraphQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (RDFFormat) returnFormat, boa);
+			} else if (op.equals(Operation.CONSTRUCT) || op.equals(Operation.DESCRIBE)) {
+				returnFormat = RDFFormat.matchMIMEType(acceptHeader, allFormats)
+						.orElseGet(new Supplier<RDFFormat>() {
+							@Override
+							public RDFFormat get() {
+								log.warn(
+										"ACCEPT header is empty or unkown. Fallback to {}.",
+										RDFFormat.JSONLD.getDefaultMIMEType());
+								return RDFFormat.JSONLD;
+							}
+						});
+				try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
+					try(RepositoryConnection con = repository.getConnection()){
+						QueryResultIO.writeGraph(con.prepareGraphQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (RDFFormat) returnFormat, boa);
+					}
+					writeResult(message, boa, returnFormat);
 				}
-				writeResult(message, boa, returnFormat);
-			}
-
-
-		} else if (op.equals(Operation.SELECT)) {
-			returnFormat = TupleQueryResultFormat.matchMIMEType(acceptHeader,
-					allFormats).orElseGet(
-					new Supplier<TupleQueryResultFormat>() {
-						@Override
-						public TupleQueryResultFormat get() {
-							log.warn(
-									"ACCEPT header is empty or unkown. Fallback to {}.",
-									TupleQueryResultFormat.JSON
-											.getDefaultMIMEType());
-							return TupleQueryResultFormat.JSON;
-						}
-					});
-			try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
-				try(RepositoryConnection con = repository.getConnection()){
-					QueryResultIO.writeTuple(con.prepareTupleQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (QueryResultFormat) returnFormat, boa);
+	
+	
+			} else if (op.equals(Operation.SELECT)) {
+				returnFormat = TupleQueryResultFormat.matchMIMEType(acceptHeader,
+						allFormats).orElseGet(
+						new Supplier<TupleQueryResultFormat>() {
+							@Override
+							public TupleQueryResultFormat get() {
+								log.warn(
+										"ACCEPT header is empty or unkown. Fallback to {}.",
+										TupleQueryResultFormat.JSON
+												.getDefaultMIMEType());
+								return TupleQueryResultFormat.JSON;
+							}
+						});
+				try(ByteArrayOutputStream boa = new ByteArrayOutputStream();){
+					try(RepositoryConnection con = repository.getConnection()){
+						QueryResultIO.writeTuple(con.prepareTupleQuery(QueryLanguage.SPARQL, sparqlQuery).evaluate(), (QueryResultFormat) returnFormat, boa);
+					}
+					writeResult(message, boa, returnFormat);
 				}
-				writeResult(message, boa, returnFormat);
-			}
-
+	
+		} else if (op.equals(Operation.UPDATE)) {
+				try(RepositoryConnection con = repository.getConnection()){
+					con.prepareUpdate(QueryLanguage.SPARQL, sparqlQuery).execute();
+				}
+			
 		}
 			
-		
 			
 		}catch(Exception e){
 			log.error("Failed to execute SPARQL query on the repository connection.", e);
--- geiser-sparql-service/src/test/java/org/aksw/geiser/sparqlservice/GeiserSparqlServiceApplicationTests.java
+++ geiser-sparql-service/src/test/java/org/aksw/geiser/sparqlservice/GeiserSparqlServiceApplicationTests.java
@@ -1,16 +1,124 @@
 package org.aksw.geiser.sparqlservice;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.query.QueryEvaluationException;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.query.TupleQueryResultHandlerException;
+import org.eclipse.rdf4j.query.resultio.QueryResultIO;
+import org.eclipse.rdf4j.query.resultio.QueryResultParseException;
+import org.eclipse.rdf4j.query.resultio.TupleQueryResultFormat;
+import org.eclipse.rdf4j.query.resultio.UnsupportedQueryResultFormatException;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@Ignore
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = { GeiserSparqlServiceApplication.class, GeiserSparqlServiceApplication.GeiserSparqlSelectService.class, GeiserSparqlServiceApplicationTests.TestConfiguration.class})
 public class GeiserSparqlServiceApplicationTests {
-
-	@Test
-	public void contextLoads() {
+	
+	@Autowired
+	private Repository repository;
+	
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    
+    private static final ValueFactory vf = SimpleValueFactory.getInstance();
+    
+    private static final IRI testPersonIRI = vf.createIRI("http://example.com/person1");
+    
+	@Before
+	public void before() throws Exception {
+//		try(RepositoryConnection con = repository.getConnection()){
+//			con.add(vf.createStatement(testPersonIRI, RDF.TYPE, FOAF.PERSON));
+//			assertEquals(1, con.size());
+//		}
+		
+	}
+	
+	@After
+	public void after() throws Exception {
+//		try(RepositoryConnection con = repository.getConnection()){
+//			con.prepareUpdate(QueryLanguage.SPARQL, "DROP ALL").execute();
+//		}
 	}
 
+
+	
+
+	@Test
+	public void testHandleServiceDiscoveryRequest() throws TupleQueryResultHandlerException, QueryEvaluationException, QueryResultParseException, UnsupportedQueryResultFormatException, IOException {
+		
+		String query = "SELECT * WHERE {?s a ?o}";
+		Message message = MessageBuilder.withBody(query.getBytes())
+				.build();
+		rabbitTemplate.send("sparql-select-v1.sparqlresult", message);
+        String stringResult = (String) rabbitTemplate.receiveAndConvert("test");
+
+        try(TupleQueryResult tr = QueryResultIO.parseTuple(IOUtils.toInputStream(stringResult, "UTF-8"), TupleQueryResultFormat.JSON)){
+            assertTrue(tr.hasNext());
+            while(tr.hasNext()){
+            	assertEquals((IRI)tr.next().getBinding("o"),testPersonIRI);
+            }
+        }
+
+        
+	}
+	
+	@Configuration
+	public static class TestConfiguration {
+
+//		@Bean
+//		public Repository repository() {
+//			Repository r =  new SailRepository(new MemoryStore());
+//			r.initialize();
+//			return r;
+//		}
+		
+		@Bean
+	    public ConnectionFactory connectionFactory() {
+	        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+	        connectionFactory.setUsername("guest");
+	        connectionFactory.setPassword("guest");
+	        return connectionFactory;
+	    }
+		
+		@Bean
+	    public RabbitTemplate rabbitTemplate() {
+	        return new RabbitTemplate(connectionFactory());
+	    }
+
+
+	    @Bean
+	    public RabbitAdmin rabbitAdmin() {
+	        return new RabbitAdmin(connectionFactory());
+	    };
+	}
+	
 }
