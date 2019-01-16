85ff5d496ebc0071adabdcaa4d1c633babfd77b5
mwauer <dr.matthias.wauer@gmail.com>
2019-01-16 12:38:34 +0100

    delay service test files

---
+++ geiser-delay-service/src/test/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Test script for sending all 6199 USU tweets to transformation service.
+# Assumes that you have a "test" routing key for receiving the
+# results.
+#
+import pika
+import gzip
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               '127.0.0.1'))
+channel = connection.channel()
+
+properties = pika.BasicProperties(content_type='application/json')
+
+print("Sending JSON message...")
+
+with gzip.open('many-test.json.gz', 'rb') as f:
+  for line in f:
+    channel.basic_publish(exchange='geiser',
+                          routing_key='delay-v1.test',
+                          body=line,
+	                  properties=properties)
+
+print(" [x] Sent JSON message to Service request message")
+connection.close()
